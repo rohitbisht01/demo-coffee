eb01238ed6fb90f54e9292ccdc32b75639543ad8
Rohit Singh Bisht <rb9519208@gmail.com>
2022-06-19 20:32:03 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function accordionFunction(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+function openModal() {
+  document.getElementById("modal").style.display = "block";
+}
